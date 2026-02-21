74177eecae001510376875403947bb9834ec3741
Nakul Kumar <nakul.kumar@aiprus.com>
2026-02-21 16:10:36 +0530

    b ugs fix firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -11,60 +16,74 @@ import Manufacturing from "./pages/Manufacturing";
 import Products from "./pages/Products";
 import ProtectedRoute from "./ProtectedRoute";
 
+function Layout() {
+  const location = useLocation();
+
+  // 👉 Login page par Navbar & Footer hide
+  const hideLayout = location.pathname === "/login";
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar */}
+      {!hideLayout && <Navbar />}
+
+      {/* Main Content */}
+      <main className="flex-grow">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/products"
+            element={
+              <ProtectedRoute>
+                <Products />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/manufacturing"
+            element={
+              <ProtectedRoute>
+                <Manufacturing />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/location"
+            element={
+              <ProtectedRoute>
+                <Location />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/contact"
+            element={
+              <ProtectedRoute>
+                <Contact />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+
+      {/* Footer */}
+      {!hideLayout && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-
-        <main className="flex-grow">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<Index />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/login" element={<Login />} />
-
-            {/* Protected Routes */}
-            <Route
-              path="/products"
-              element={
-                <ProtectedRoute>
-                  <Products />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/manufacturing"
-              element={
-                <ProtectedRoute>
-                  <Manufacturing />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/location"
-              element={
-                <ProtectedRoute>
-                  <Location />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/contact"
-              element={
-                <ProtectedRoute>
-                  <Contact />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </main>
-
-        <Footer />
-      </div>
+      <Layout />
     </Router>
   );
 }
